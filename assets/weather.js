0bf57fc7747d0b24f9f006b5fe49581b5c33ba7b
Joshua Almodovar <joshuarosamktp4@gmail.com>
2021-06-03 00:13:12 -0400

    and search bar

---
+++ assets/weather.js
@@ -0,0 +1,8 @@
+var cities = []
+var citySearchEl = document.querySelector("#city-name")
+
+
+/*fetch(
+    "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
+    )
+    console.log(fetch)*/
