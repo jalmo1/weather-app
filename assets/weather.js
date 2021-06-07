0de958bd7b3627f45ae65e53ecb31eeda3cae634
Joshua Almodovar <joshuarosamktp4@gmail.com>
2021-06-06 21:19:03 -0400

    add uv index and icons

--- assets/weather.js
+++ assets/weather.js
@@ -10,12 +10,19 @@ THEN I am presented with a 5-day forecast that displays the date, an icon repres
 WHEN I click on a city in the search history
 THEN I am again presented with current and future conditions for that city
 */
-var city = "";
-var previousCity = "";
+var temp = document.querySelector("#tempEl");
+var wind = document.querySelector("#windEl");
+var humid = document.querySelector("#humidEl");
+var UvI = document.querySelector("#uvIndexEl");
+var currentCityDisplay = document.querySelector("#city");
+var dateDisplay = document.querySelector("#date");
+var cityHistory = {};
 var searchBtn = document.querySelector("#searchBtn");
 var citySearchInputEl = document.getElementById("city-search-input");
 var fiveDay = document.getElementById("5dayforecast");
 var currentWeather = document.querySelector(".current-weather");
+var currentDate = moment().format("MMMM Do YYYY");
+console.log(currentDate);
 
 $("#searchBtn").click(function (event) {
   event.preventDefault();
@@ -25,14 +32,13 @@ $("#searchBtn").click(function (event) {
   } else {
     localStorage.setItem("city-search-input", citySearchInputEl.value);
     getCity();
+
+    //getUvIndex();
   }
   console.log(citySearchInputEl.value);
-  console.log("City searched");
 });
 
 function getCity(searchedCity) {
-  var apiUrl =
-    "https://api.openweathermap.org/data/2.5/weather?q=searchedCity&units=imperial&appid=";
   var apiKey = "4d3c063e98523c8c6f0e9acf35c7a45f";
 
   fetch(
@@ -41,8 +47,72 @@ function getCity(searchedCity) {
       "&units=imperial&appid=" +
       apiKey
   ).then(function (response) {
-    response.json().then(function (data) {
-      console.log(response, data);
-    });
+    response
+      .json()
+      .then(function (data) {
+        console.log(response, data);
+        //pulls the data from the API
+        /*var cityArr = {};
+        console.log(cityArr);
+        cityArr.city = data.name;
+        cityHistory.push(cityArr);
+        saveCity("cities", JSON.stringify(cityHistory));*/
+
+        //let uvApi = `https://api.openweathermap.org/data/2.5/uvi?lat=${cityArr.cityUVIndex.lat}&lon=${cityObj.cityUVIndex.lon}&APPID=${apiKey}&units=imperial`;
+        $("#tempEl").text("Temperature: " + Math.floor(data.main.temp));
+        $("#windEl").text("Wind: " + Math.floor(data.wind.speed));
+        $("#humidEl").text("Humidity: " + Math.floor(data.main.humidity));
+        return data;
+      }) //clears content and displays new city/date
+      .then(function displayCityDate(data) {
+        currentCityDisplay.textContent = "";
+        dateDisplay.textContent = "";
+        $("#city").append(citySearchInputEl.value);
+        $("#date").append(currentDate);
+        return fetch(
+          "https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" +
+            data.coord.lat +
+            "&lon=" +
+            data.coord.lon +
+            "&appid=4d3c063e98523c8c6f0e9acf35c7a45f"
+        ).then(function (response) {
+          return response.json();
+        });
+      })
+      .then(function getUvIndex(data) {
+        console.log(data);
+        $("#uvIndexEl").text("UV Index: " + data.current.uvi);
+
+        if (data.current.uvi <= 2) {
+          $("#uvIndexEl").addClass("moderate");
+        }
+        return data;
+      })
+      .then(function getIcon(data) {
+        var weatherIcon =
+          "http://openweathermap.org/img/wn/" +
+          data.current.weather[0].icon +
+          ".png";
+        console.log(data.current.weather[0].icon);
+        $("#weatherIcon").attr("src", weatherIcon);
+        return data;
+      })
+      .then(function fiveDay(data) {
+        for (var i = 1; i < 6; i++) {
+          var temp = data.daily[i].temp.day;
+          var humidity = data.daily[i].humidity;
+          var wind = data.daily[i].wind_speed;
+          var uvIndex = data.daily[i].uvi;
+          var weatherIcon =
+            "http://openweathermap.org/img/wn/" +
+            data.daily[i].weather[0].icon +
+            ".png";
+          //createFiveDay();
+        }
+      });
   });
+}
+
+function saveCity(key, value) {
+  localStorage.setItem(key, value);
 }
